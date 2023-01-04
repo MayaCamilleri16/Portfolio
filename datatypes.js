3436979b178b1b93ee5ec34d39605b15e0fd500c
MayaCamilleri16 <91874242+MayaCamilleri16@users.noreply.github.com>
2023-01-04 11:25:37 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+//string, i.e. a number of characters
+let fullname = "Maya";
+console.log(fullname);
+
+
+
+//Number
+let age = 18
+console.log(age);
+age = "Maya";
+console.log()
+
+//overriding  data types
+let a =1, b =2, c=a+b;
+console.log(c);
+c = "a";
+let d = c+a;
+console.log(d);
